96e3b6d92994f919e3072a268527b96d54ce5442
Royce Chua <royce236@gmail.com>
2021-05-12 02:19:09 +0800

    Remove unecessary code

--- template/src/routes/routesContainer.jsx
+++ template/src/routes/routesContainer.jsx
@@ -5,25 +5,12 @@ import Dashboard from "../pages/dashboard/Dashboard";
 import Users from '../pages/users/Users';
 import NotFound from "../pages/NotFound";
 
-// const Dashboard = React.lazy(() => import("../pages/dashboard/Dashboard"));
-// const Iviewer = React.lazy(() => import("../pages/iviewer/Iviewer"));
-// const HireIQ = React.lazy(() => import("../pages/hireiq/hireiq"));
-// const TalkPush = React.lazy(() => import("../pages/talkpush/talkpush"));
-// const ShowTalkPushApplicant = React.lazy(() =>
-//   import("../pages/talkpush/components/ShowTalkPushApplicant")
-// );
-// const NotFound = React.lazy(() => import("../pages/NotFound"));
-
 const routesContainer = () => {
   return (
     <Switch>
       {/* <Suspense fallback={<div>Loading...</div>}> */}
         <Route path="/dashboard" exact component={Dashboard} />
         <Route path="/users" exact component={Users} />
-        {/* <Route path="/iviewer" component={Iviewer} />
-        <Route path="/hireiq" component={HireIQ} />
-        <Route exact path="/talkpush" component={TalkPush} />
-        <Route path="/talkpush/:id" component={ShowTalkPushApplicant} /> */}
         <Route component={NotFound}/>
       {/* </Suspense> */}
     </Switch>
